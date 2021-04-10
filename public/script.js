90ae04e00d260a601eeece322dd5b763a0555ee0
shrutisriram10 <78097685+shrutisriram10@users.noreply.github.com>
2021-04-09 21:42:03 -0400

    working chart

--- public/script.js
+++ public/script.js
@@ -1,24 +1,104 @@
-async  function dininghalls(){
-    const request = await fetch('api/dining');
-    const data = await request.json();
-    console.log(data);
-    const diningdata = data.data;
-    const target = document.querySelector('tbody');
-    
-    diningdata.forEach((item) => {
-        const hallID = item.hall_id;
-        const hallname = item.hall_name;
-        const halllocation = item.hall_address;
-        const tablerow = document.createElement('tr');
-        tablerow.innerHTML = `
-                <th>${hallID}</th>
-                <td>${hallname}</td>
-                <td>${halllocation}</td>
-            `;
-        target.append(tablerow);
-    })
+async function windowActions() {
+  const total_calories = [];
+  const total_serving = [];
+  const total_chol = [];
+  const total_sodium = [];
+  const total_carbs = [];
+  const total_protein = [];
+  const total_fat = [];
+
+  //Requesting dining and macros data
+  const diningRequest = await fetch('/api/dining');
+  const hallData = await diningRequest.json();
+  const tableBody = document.querySelector('tbody');
+  const macrosRequest = await fetch('api/macros');
+  const macrosData = await macrosRequest.json();
+
+  hallData.data.forEach((hall) => {
+    const tableLine = document.createElement('tr');
+    tableLine.innerHTML = `
+    <th>${hall.hall_id}</th>
+    <td>${hall.hall_name}</td>
+    <td>${hall.hall_address}</td>`;
+    tableBody.append(tableLine);
+  });
+
+  const chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    title: {
+      text: 'Meals and Macros'
+    },
+    toolTip: {
+      shared: true
+    },
+    legend: {
+      cursor: 'pointer',
+      itemclick: toggleDataSeries
+    },
+    data: [
+      {
+        type: 'stackedBar',
+        name: 'Calories',
+        showInLegend: 'true',
+        dataPoints: total_calories
+      },
+      {
+        type: 'stackedBar',
+        name: 'Serving Size',
+        showInLegend: 'true',
+        dataPoints: total_serving
+      },
+      {
+        type: 'stackedBar',
+        name: 'Cholesterol',
+        showInLegend: 'true',
+        dataPoints: total_chol
+      },
+      {
+        type: 'stackedBar',
+        name: 'Sodium',
+        showInLegend: 'true',
+        dataPoints: total_sodium
+      },
+      {
+        type: 'stackedBar',
+        name: 'Carbs',
+        showInLegend: 'true',
+        dataPoints: total_carbs
+      },
+      {
+        type: 'stackedBar',
+        name: 'Protein',
+        showInLegend: 'true',
+        dataPoints: total_protein
+      },
+      {
+        type: 'stackedBar',
+        name: 'Fat',
+        showInLegend: 'true',
+        dataPoints: total_fat
+      }
+    ]
+  });
+  macrosData.forEach((meal) => {
+    total_calories.push({label: meal.meal_name, y: meal.calories});
+    total_serving.push({label: meal.meal_name, y: meal.serving_size});
+    total_chol.push({label: meal.meal_name, y: meal.cholesterol});
+    total_sodium.push({label: meal.meal_name, y: meal.sodium});
+    total_carbs.push({label: meal.meal_name, y: meal.carbs});
+    total_protein.push({label: meal.meal_name, y: meal.protein});
+    total_fat.push({label: meal.meal_name, y: meal.fat});
+  });
+  chart.render();
+
+  function toggleDataSeries(e) {
+    if (typeof e.dataSeries.visible === 'undefined' || e.dataSeries.visible) {
+      e.dataSeries.visible = false;
+    } else {
+      e.dataSeries.visible = true;
+    }
+    chart.render();
+  }
 }
-async function windowActions(){
-    dininghalls();
-}
-window.onload=windowActions;
+
+window.onload = windowActions();
